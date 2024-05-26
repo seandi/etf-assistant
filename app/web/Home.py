2a6e662ee6d24438def7a6f28f8a7eaca49b261b
seandi <eandi.simo@outlook.it>
2024-05-26 16:28:31 +0200

    tweak homepage

--- app/web/Home.py
+++ app/web/Home.py
@@ -5,7 +5,6 @@ import os
 from app.web.utils import load_etf_db
 from app.web.ui import make_searchbar
 
-
 st.set_page_config(layout="centered")
 
 load_dotenv(override=True)
@@ -17,5 +16,8 @@ with st.spinner():
 
 
 # UI layout
-st.title("ETF Assistant")
-make_searchbar(etf_df, name="home")
+original_title = '<p style="font-family:sans-serif; color:#006989; font-size: 60px; text-align:center; font-weight:bold;">ETF Assistant</p>'
+st.markdown(original_title, unsafe_allow_html=True)
+
+with st.container(border=True):
+    make_searchbar(etf_df, name="home")
